e17d853c6739e9432aec82a7ffb818fd55fe3a9a
Vaalley <vaaleyou@gmail.com>
2023-04-18 22:16:40 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '../components/Header.svelte';
+</script>
+
+<svelte:head>
+	<title>Recreating Lobe</title>
+	<!-- <link rel="icon" href="" /> -->
+</svelte:head>
+
+<body>
+	<Header />
+</body>
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			screens: {
+				// '-2xl': { max: '1439px' },
+				'-xl': { max: '1279px' },
+				'-lg': { max: '1023px' },
+				'-md': { max: '767px' },
+				'-sm': { max: '639px' }
+			},
+			colors: {
+				primary: '#04ddb2',
+				dark: '#767676',
+				darker: '#333',
+				white: '#fff',
+			},
+			fontFamily: {}
+		}
+	},
+	plugins: []
+};
